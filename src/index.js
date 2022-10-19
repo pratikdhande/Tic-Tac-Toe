3b35ba4aef2afeae47bf6cce1fc7ed3598f5f0a8
pratikdhande <pratikdhande99@gmail.com>
2022-10-20 02:38:22 +0530

    App.js, components and other files added

--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
+import 'bootstrap/dist/css/bootstrap.css';
+import './App.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
